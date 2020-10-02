8d6a3254d6273f05d81ead26eae77ded996d459c
sali73 <sally.elgendy@hotmail.com>
2020-10-02 15:16:42 -0400

    chenge fields

--- fligthscheduler/api/models.py
+++ fligthscheduler/api/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.utils.timezone import now
 
 class Flight(models.Model):
     flight = models.CharField(max_length=10, null=True, blank=True)
-    date = models.DateTimeField(default=now, editable=False)
+    date = models.CharField(max_length=12, null=True, blank=True)
     destination = models.CharField(max_length=100, null=True, blank=True)
     scheduled_departure_time = models.CharField(max_length=10)
     assigned_plane_type = models.CharField(max_length=400)
